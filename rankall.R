dfb50d4e4bcb5ab100407135327eb63f4e197c4c
Monica <mntjones@users.noreply.github.com>
2015-06-28 10:54:04 -0700

    Create rankall.R

---
+++ rankall.R
@@ -0,0 +1,30 @@
+library(dplyr)
+rankall <- function (outcome, num = "best") {
+    source("ranked.R")
+    rank <- ranked(outcome)
+    state_list <- unique(rank[,2])
+    rank_vector <- c()
+    rownum <- num
+    outCol <- switch(outcome,
+                     "heart attack" = as.numeric(3),
+                     "heart failure" = as.numeric(4),
+                     "pneumonia" = as.numeric(5)
+    )
+
+    for (i in 1:length(state_list)) {
+        st_data <- rank[grep(state_list[i],rank$state),]
+        print(st_data)
+        orderdata <- st_data[order(st_data[,outCol]),]
+        
+        if (num == "best") {
+            rownum = 1
+        }
+        if (num == "worst") {
+            rownum = as.numeric(nrow(orderdata))
+        }
+        
+        rank_vector <- append(rank_vector, as.character(orderdata[rownum,1]))
+        rank_vector <- append(rank_vector, as.character(state_list[i]))
+        print(rownum)
+        rownum <- num
+    }
